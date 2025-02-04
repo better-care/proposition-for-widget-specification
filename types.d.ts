850618b0b73fb9dff5ac53b0303d70812d6e4353
Metod Ribič <metod.ribic@better.care>
2025-02-04 10:52:08 +0100

    feat: add types for widget descriptor

---
+++ types.d.ts
@@ -0,0 +1,78 @@
+export interface WidgetModel {
+  /* Data model of the widget */
+  inputData?: ValueModel;
+
+  /* Configuration of the EHR Studio */
+  config?: Configuration;
+
+  /* This is the representation of the widget data model which can be configured inside the Widget confiruator in
+   * EHR Studio */
+  descriptor: WidgetDescriptor;
+
+  /* this flag is provided from the EHR Studio, and it indicates that the mocked data should be populated to widget so
+   * it can be shown in various editors e.g.: Form builder */
+  useDefaultData?: boolean;
+
+  /* This is primarily used only for widgets that are representing any field that should be represented with a value in
+   * an openEHR composition. When outputEvent data is emitted the value is automatically assigned to composition */
+  outputData?: Event;
+
+  /* This is the event that is sent to the form-renderer where you can react to it */
+  outputAction?: Event;
+}
+
+export type BaseValueModel = {
+  type: ValueModelType;
+  alias?: string;
+  defaultValue?: ValueModelType;
+  metadata?: Metadata;
+  required?: boolean;
+  input?: string[];
+};
+
+export type PrimitiveValueModel = BaseValueModel & {
+  values?: Array<string | number | boolean | bigint | symbol | null | undefined>;
+};
+
+export type ObjectValueModel = BaseValueModel & {
+  objectModel?: Record<string, ValueModel>;
+}
+
+export type ListValueModel = BaseValueModel & {
+  listModel?: ValueModel;
+}
+
+export type ValueModel = PrimitiveValueModel | ObjectValueModel | ListValueModel;
+
+export type Configuration = {
+  [key: string]: {
+    type: ConfigurationType;
+    defaultValue: ConfigurationType;
+    metadata?: Metadata;
+  };
+};
+
+export interface Condition {
+  field: string;
+  targets: string[];
+  actions: ConditionAction[];
+}
+
+export interface ConditionAction {
+  eq?: string | boolean;
+  state: string[];
+}
+
+export type Metadata = {
+  title: string;
+  description?: string
+};
+
+export type WidgetDescriptor = {
+  valueModel: ValueModel;
+  configuration: Configuration;
+  $conditions?: Condition[];
+};
+
+export type ValueModelType = 'object' | 'list' | 'string' | 'number';
+export type ConfigurationType = 'boolean' | 'enum' | 'number' | 'string';
